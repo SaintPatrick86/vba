f4bd55f912892d746afc5d392b38c7ef62eb68c6
SaintPatrick86 <patrick.moch@gmail.com>
2019-09-04 09:17:49 +0200

    Update PMIT_Functions.vb

--- Kursdatenversorgung/PMIT_Functions.vb
+++ Kursdatenversorgung/PMIT_Functions.vb
@@ -1,3 +1,4 @@
+
 Public Function DateiName() As String
 Dim f As Office.FileDialog
 Set f = Application.FileDialog(msoFileDialogFilePicker)
@@ -7,4 +8,17 @@ f.Show
 If f.SelectedItems.Count > 0 Then ' Prüfen auf 'Abbrechen'-Button
     DateiName = f.SelectedItems(1)
 End If
+End Function
+
+Public Function Zeilenende(tabelle As String) As String
+Dim loErste As Long, loLetzte As Long
+
+With Worksheets(tabelle)
+    If .FilterMode Then .ShowAllData
+    loErste = .Cells(.Rows.Count, 9).End(xlUp).Offset(1, 0).Row
+    loLetzte = .Cells(.Rows.Count, 1).End(xlUp).Row
+End With
+
+Zeilenende = loLetzte
+
 End Function
